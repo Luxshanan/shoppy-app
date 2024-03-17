5a0d04a534e3bd8cbd1e0cc68a3aac56f31cbcf5
Luxshanan <68101037+Luxshanan@users.noreply.github.com>
2024-03-17 17:13:47 +0530

    axios config update

--- src/AxiosConfig.js
+++ src/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://shoppyservice-854388473:8080/api",
+    baseURL: "https://5d7cdc4e-3fa4-47d5-a36b-52141669a3ee-dev.e1-us-cdp-2.choreoapis.dev/zyhg/shoppyservice/shoppy-api-5c6/v1.0/api",
     headers: {
         'API-Key': 'key'
     }
