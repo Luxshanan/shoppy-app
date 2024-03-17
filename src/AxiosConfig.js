856ff5b9800bed2e82b6955a3001901bad4b2291
Luxshanan <68101037+Luxshanan@users.noreply.github.com>
2024-03-17 16:37:53 +0530

    minor update

--- src/AxiosConfig.js
+++ src/AxiosConfig.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: "http://shoppyws-2108198921:8080/api",
     headers: {
-        'API-Key': process.env.REACT_APP_API_KEY
+        'API-Key': 'key'
     }
 });
